d0bb4d5329016000bab7a77f280ab17fb00522f1
steventhesatch <steventhesatch@e679fa96-29ae-11df-bafe-d99efc0dbc91>
2010-04-30 04:13:05 +0000

    Initial checkin of MockServer

---
+++ MockServer/src/MainServlet.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
+	    String docType =
+	      "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
+	      "Transitional//EN\">\n";
+	    out.println(docType +
+	                "<HTML>\n" +
+	                "<HEAD><TITLE>Hello</TITLE></HEAD>\n" +
+	                "<BODY BGCOLOR=\"#FDF5E6\">\n" +
+	                "<H1>"+request.getRequestURI()+"</H1>\n" +
+	                "<IMG src=\"http://www.google.com/images/nav_logo8.png\" />" +
+	                "</BODY></HTML>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
