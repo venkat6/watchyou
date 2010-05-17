8e32f61d89eb4c29441a73f76db30ef3760c4f6e
steventhesatch <steventhesatch@e679fa96-29ae-11df-bafe-d99efc0dbc91>
2010-05-17 15:35:40 +0000

    Updates to GUI and subtle changes to the sniffer

--- trunk/src/gui/MainFrame.java
+++ trunk/src/gui/MainFrame.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
@@ -29,7 +30,7 @@ public class MainFrame extends JFrame {
 	private static MainFrame _instance;
 	private JPanel jContentPane = null;
 	private MozillaPanel browserPanel = null;
-	private JPanel statsPanel = null;
+	private Home statsPanel = null;
 
 	/**
 	 * This is the default constructor
@@ -78,7 +79,7 @@ public class MainFrame extends JFrame {
 			try {
 				browserPanel = new MozillaPanel();
 				//browserPanel.setBorder(BorderFactory.createTitledBorder(new EtchedBorder(EtchedBorder.RAISED), "Embedded browser"));
-				browserPanel.setSize(500, 400);
+				//browserPanel.setMinimumSize(new Dimension(500, 800));
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -86,6 +87,22 @@ public class MainFrame extends JFrame {
 		}
 		return browserPanel;
 	}
+	
+	/**
+	 * This method initializes statsPanel	
+	 * 	
+	 * @return javax.swing.JPanel	
+	 */
+	public Home getStatsPanel() {
+		if (statsPanel == null) {
+			statsPanel = new Home();
+			//statsPanel.setLayout(new GridBagLayout());
+		}
+		return statsPanel;
+	}
+	
+	
+	/* Public access methods for the sniffer */
 	
 	public void navigate(String url) throws MalformedURLException
 	{
@@ -96,66 +113,5 @@ public class MainFrame extends JFrame {
 	{
 		return _instance;
 	}
-
-	/**
-	 * This method initializes statsPanel	
-	 * 	
-	 * @return javax.swing.JPanel	
-	 */
-	private JPanel getStatsPanel() {
-		if (statsPanel == null) {
-			statsPanel = new Home();
-			//statsPanel.setLayout(new GridBagLayout());
-		}
-		return statsPanel;
-	}
-
-	public static void main(String[] args) throws Exception
-	{
-		//frame.setBorder(BorderFactory.createTitledBorder(new EtchedBorder(EtchedBorder.RAISED), "Embedded browser"));
-		//frame.navigate("http://www.stevenssite.com");
 		
-		/* ----- start jnetpcap ------ */
-		/*
-		HttpAnalyzer httpAnalyzer = JRegistry.getAnalyzer(HttpAnalyzer.class);
-        httpAnalyzer.add( new HttpParser() );
-        StringBuilder errbuf = new StringBuilder();
-        
-        List<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs
-        int r = Pcap.findAllDevs(alldevs, errbuf);
-		if (r == Pcap.NOT_OK || alldevs.isEmpty()) {
-			System.err.printf("Can't read list of devices, error is %s", errbuf
-			    .toString());
-			return;
-		}
-
-		System.out.println("Network devices found:");
-
-		int i = 0;
-		for (PcapIf device : alldevs) {
-			String description =
-			    (device.getDescription() != null) ? device.getDescription()
-			        : "No description available";
-			System.out.printf("#%d: %s [%s]\n", i++, device.getName(), description);
-		}
-
-		PcapIf device = alldevs.get(0); // We know we have atleast 1 device
-		System.out
-		    .printf("\nChoosing '%s' on your behalf:\n",
-		        (device.getDescription() != null) ? device.getDescription()
-		            : device.getName());
-		
-		//Pcap pcap= Pcap.openLive("\\Device\\NPF_{7487DF0E-2B06-4660-9ED2-6E57E57094AA}", 65535, 1, 4444, errbuf); // wireless
-        Pcap pcap= Pcap.openLive("\\Device\\NPF_{56F5978B-5E2C-4FFF-9CD1-C6D8124A20D3}", 65535, 1, 4444, errbuf); // ethernet
-        //Pcap pcap= Pcap.openOffline("specific.pcap", errbuf);
-        if (pcap == null) {
-                System.err.println(errbuf.toString());
-                return;
-        }
-        
-        pcap.loop(Pcap.LOOP_INFINATE, JRegistry.getAnalyzer(JController.class), null);
-        
-        pcap.close();
-        */
-	}
 }
